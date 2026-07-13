2014a5e9fcc1063767dfe7c95b848f0900859adf
Ruben-1967 <rubenmarcosgonzalezerazo@gmail.com>
2026-07-13 17:57:50 -0400

    Eliminar archivo duplicado prisma/prisma.js

--- prisma/prisma.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-
-// Reutilizamos una sola instancia de PrismaClient en toda la app,
-// en vez de crear una nueva conexión por request.
-const prisma = new PrismaClient();
-
-module.exports = prisma;
