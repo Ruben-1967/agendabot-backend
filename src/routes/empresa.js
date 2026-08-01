fe49ea3757f32e988f4ee829ecd7f3ea4f7281cf
Ruben-1967 <rubenmarcosgonzalezerazo@gmail.com>
2026-08-01 19:49:49 -0400

    feat: Agregar tonoComunicacion a empresas

--- src/routes/empresa.js
+++ src/routes/empresa.js
@@ -15,7 +15,7 @@ const router = express.Router();
 const prisma = require('../lib/prisma');
 const { requireAuth, requireRole } = require('../middleware/auth');
 
-const CAMPOS_INFO = ['direccion', 'notaAgendamiento', 'informacionAdicional', 'requiereRut'];
+const CAMPOS_INFO = ['direccion', 'notaAgendamiento', 'informacionAdicional', 'requiereRut', 'tonoComunicacion'];
 
 router.get('/info', requireAuth, requireRole('ADMIN'), async (req, res) => {
   try {
